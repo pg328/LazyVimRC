cbb18f8c6c9db2680b2edde76442aa3e8d43baa7
Phil George <23001002+pg328@users.noreply.github.com>
2023-12-07 16:02:09 +0000

    Initial Commit :)

---
+++ lua/config/lazy.lua
@@ -0,0 +1,101 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim",                                    import = "lazyvim.plugins" },
+    -- change some telescope options and a keymap to browse plugin files
+    {
+      "nvim-telescope/telescope.nvim",
+      keys = {
+        -- add a keymap to browse plugin files
+        -- stylua: ignore
+        {
+          "<leader>fp",
+          function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+          desc = "Find Plugin File",
+        },
+      },
+      -- change some options
+      opts = {
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          winblend = 0,
+        },
+      },
+    },
+    {
+      "telescope.nvim",
+      dependencies = {
+        'nvim-telescope/telescope-file-browser.nvim', -- NetRW replacement,
+        build = "make",
+        config = function()
+          require("telescope").load_extension("file_browser")
+          vim.opts.set('n', '<leader>e',
+            '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), respect_git_ignore = false, hide_parent_dir = true, git_status = false,  hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = { height = 40 }})<CR>',
+            { noremap = true, silent = true, desc = "File Browser" })
+        end,
+      }
+    },
+    -- add telescope-fzf-native
+    {
+      "telescope.nvim",
+      dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      }
+    },
+    { -- add symbols-outline
+      "simrat39/symbols-outline.nvim",
+      cmd = "SymbolsOutline",
+      keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+      config = true,
+    }, -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+  },
+  install = { colorscheme = { "catppuccin", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+vim.cmd.colorscheme "catppuccin"
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  config = function()
+    return {
+      event_handlers = {
+
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+      }
+    }
+  end
+}
