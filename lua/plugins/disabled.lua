e6bbfd3cdab293d74175720a139fa161de6c6da6
Philip George <23001002+pg328@users.noreply.github.com>
2024-12-21 16:28:29 +0000

    upgrade: Replace NeoTree with Oil.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "folke/flash.nvim", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
-  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false }
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
