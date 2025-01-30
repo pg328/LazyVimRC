b8b6a82d8cae162c7a9d1e7c4f598463fcfc5b61
Philip George <23001002+pg328@users.noreply.github.com>
2025-01-30 21:59:22 +0000

    upgrade: Add PanDoc mappings

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.md",
+  callback = function()
+    vim.keymap.set("n", "<leader>md", function()
+      require("auto-pandoc").run_pandoc()
+    end, { silent = true, buffer = 0 })
+  end,
+  group = vim.api.nvim_create_augroup("setAutoPandocKeymap", {}),
+  desc = "Set keymap for auto-pandoc",
+})
